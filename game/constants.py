2c4f0a3d4f78f0de8eef294a8555e8bfc72d8f7b
AgentAndrew <agentandrewminecraft@gmail.com>
2024-02-28 21:48:28 -0600

    init

---
+++ game/constants.py
@@ -0,0 +1,10 @@
+# the default width / height of the window
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# the minimum width / height of the window
+MIN_WIDTH = 400
+MIN_HEIGHT = 300
+
+# colours
+BLACK = (0, 0, 0)
