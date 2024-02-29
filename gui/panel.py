158c60c0cb5d92a358c1a461c2a12a643dd5d457
Agent Andrew <agentandrewminecraft@gmail.com>
2024-02-29 15:24:21 -0600

    added panel

---
+++ gui/panel.py
@@ -0,0 +1,21 @@
+import pygame
+
+from .drawnobject import DrawnObject
+from .constants import BLACK
+
+
+class Panel(DrawnObject):
+    def __init__(self) -> None:
+        super().__init__()
+        self.update()
+
+    def update(self) -> None:
+        self.x = self.x_padd + self.board_size + self.padd
+        self.y = self.y_padd
+        self.width = self.square_size
+        self.height = self.board_size
+
+    def draw(self, screen: pygame.surface.Surface) -> None:
+        pygame.draw.rect(
+            screen, BLACK, (self.x, self.y, self.width, self.height), self.line_size
+        )
