18ceaa234e50baa555bef726d69c8eb060bf90df
AgentAndrew <agentandrewminecraft@gmail.com>
2024-02-28 20:56:27 -0600

    init

---
+++ game/drawnobject.py
@@ -0,0 +1,32 @@
+class DrawnObject:
+    _instances = set()
+    ratio = 4 / 3
+
+    def __init__(self) -> None:
+        self._instances.add(self)
+
+    @classmethod
+    def set_sizes(cls, screen_width: int, screen_height: int) -> None:
+        # if the width is bigger than the ratio, use the height to calculate the width
+        if screen_width >= screen_height * cls.ratio:
+            height = screen_height
+            width = round(height * cls.ratio)
+        # otherwise use the width to calculate height
+        else:
+            width = screen_width
+            height = round(width / cls.ratio)
+
+        # use the height and width to calculate other sizes
+        cls.padd = round(width / 32)
+        cls.square_size = cls.padd * 2
+        cls.board_size = cls.square_size * 8
+
+        # calculate the x_padd and y_padd based on if the height or width was adjusted
+        # add extra x_padd or y_padd depending on if the width or height is bigger than ratio
+        cls.x_padd = cls.padd + (screen_width - width) // 2
+        cls.y_padd = cls.padd + (screen_height - height) // 2
+
+        # if instance has an update method call it
+        for instance in cls._instances:
+            if hasattr(instance, "update"):
+                instance.update()
