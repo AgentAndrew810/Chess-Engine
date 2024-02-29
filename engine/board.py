f9090c680765d93ba0bb77eee25b8ebee1730785
Agent Andrew <agentandrewminecraft@gmail.com>
2024-02-29 12:30:14 -0600

    changed it so the board is the starting_board by default

--- engine/board.py
+++ engine/board.py
@@ -1,15 +1,7 @@
-from __future__ import annotations
-
 from .constants import STARTING_BOARD
 
 
 class Board:
-    def __init__(self, board: str, white_move: bool) -> None:
-        self.board = board
-        self.white_move = white_move
-
-    def starting_board(self) -> Board:
-        board = STARTING_BOARD
-        white_move = True
-
-        return Board(board, white_move)
+    def __init__(self) -> None:
+        self.board = STARTING_BOARD
+        self.white_move = True
