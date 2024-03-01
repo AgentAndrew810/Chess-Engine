6dfd8549a42208aed9c191878b7f13317d3c49bb
AgentAndrew <agentandrewminecraft@gmail.com>
2024-02-29 22:06:35 -0600

    added offsets to constants

--- engine/constants.py
+++ engine/constants.py
@@ -7,3 +7,14 @@ STARTING_BOARD = (
     + " RNBQKBNR "
     + " " * 20
 )
+
+C_OFFSETS = (-10, -1, 1, 10)
+D_OFFSETS = (-11, -9, 9, 11)
+
+OFFSETS = {
+    "N": (-21, -19, -12, -8, 8, 12, 19, 21),
+    "B": D_OFFSETS,
+    "R": C_OFFSETS,
+    "Q": C_OFFSETS + D_OFFSETS,
+    "K": C_OFFSETS + D_OFFSETS,
+}
