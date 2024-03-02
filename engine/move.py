d187cd925e446ccffab7b2675f44f35a9225ac99
AgentAndrew <agentandrewminecraft@gmail.com>
2024-03-01 21:03:26 -0600

    set promotion to default to false

--- engine/move.py
+++ engine/move.py
@@ -1,5 +1,7 @@
 class Move:
-    def __init__(self, pos: int, dest: int, capture: bool, promotion: bool) -> None:
+    def __init__(
+        self, pos: int, dest: int, capture: bool, promotion: bool = False
+    ) -> None:
         self.pos = pos
         self.dest = dest
         self.capture = capture
