83f0054db3cf39d92e269a8fb04c76463155c19f
Agent Andrew <agentandrewminecraft@gmail.com>
2024-03-05 15:38:47 -0600

    simplified code + change folder name + added held piece class

--- controller.py
+++ controller.py
@@ -1,29 +1,23 @@
 import pygame
 
-import gui
+import game
 import engine
+import utils
 
 
-class GameController(gui.DrawnObject):
+class GameController(game.DrawnObject):
     def __init__(self) -> None:
         super().__init__()
 
-        self.board_representation = engine.Board()
-        self.board = gui.Board()
-        self.panel = gui.Panel()
+        self.board = engine.Board()
+        self.board_gui = game.Board()
+        self.panel = game.Panel()
+        self.held_piece = game.HeldPiece()
 
-        self.held_piece = None
         self.player_is_white = True
         self.white_pov = not self.player_is_white
         self.x_offset = 0
         self.y_offset = 0
-
-    def get_rank_file(self, pos: int) -> tuple[int, int]:
-        rank, file = divmod(pos, 10)
-        return rank - 2, file - 1
-
-    def flip_rank_and_file(self, rank: int, file: int) -> tuple[int, int]:
-        return (rank, file) if self.white_pov else (7 - rank, 7 - file)
 
     def grab_piece(self, x: int, y: int) -> None:
         # check if the mouse is outside the board
@@ -35,30 +29,31 @@ class GameController(gui.DrawnObject):
         # get the rank and file grabbed and their offsets
         rank, self.y_offset = divmod(y - self.y_padd, self.square_size)
         file, self.x_offset = divmod(x - self.x_padd, self.square_size)
+        rank, file = utils.flip_coordinates(rank, file, self.white_pov)
 
-        # flip rank and file if playing as black
-        rank, file = self.flip_rank_and_file(rank, file)
-        piece = self.board_representation.board[(rank + 2) * 10 + file + 1]
+        # get the piece and position
+        pos = utils.get_pos(rank, file)
+        piece = self.board.board[pos]
 
         # check if grabbing the correct colour
-        if piece.isupper() == self.board_representation.white_move:
-            self.held_piece = (rank, file)
+        if piece.isupper() == self.board.white_move:
+            self.held_piece.grab(rank, file, piece)
 
     def drop_piece(self, x: int, y: int) -> None:
         # check if the mouse is outside the board
         if not (self.x_padd < x < self.x_padd + self.board_size):
-            self.held_piece = None
+            pass
         if not (self.y_padd < y < self.y_padd + self.board_size):
-            self.held_piece = None
+            pass
 
-        self.held_piece = None
+        self.held_piece.drop()
 
     def draw(self, screen: pygame.surface.Surface) -> None:
-        screen.fill(gui.BLACK)
+        screen.fill(game.BLACK)
 
-        self.board.draw(
+        self.board_gui.draw(
             screen,
-            self.board_representation,
+            self.board,
             self.white_pov,
             self.held_piece,
             self.x_offset,
