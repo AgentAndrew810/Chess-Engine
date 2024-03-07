23f6d9a41db5150b44cfc17f5dad9dc52d932fbc
Agent Andrew <agentandrewminecraft@gmail.com>
2024-03-07 15:21:08 -0600

    added storing next moves of held piece

--- game/heldpiece.py
+++ game/heldpiece.py
@@ -1,15 +1,27 @@
+import engine
+import utils
+
+
 class HeldPiece:
     def __init__(self) -> None:
-        self.holding = False
+        self.drop()
 
-    def grab(self, rank: int, file: int, piece: str) -> None:
+    def grab(
+        self, rank: int, file: int, piece: str, next_moves: list[engine.Move]
+    ) -> None:
         self.holding = True
         self.rank = rank
         self.file = file
         self.piece = piece
+        self.moves = [
+            move.dest
+            for move in next_moves
+            if utils.get_coords(move.pos) == self.position
+        ]
 
     def drop(self) -> None:
         self.holding = False
+        self.moves = []
 
     @property
     def position(self) -> tuple[int, int]:
