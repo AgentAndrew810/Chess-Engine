63b953631e394330c9a23633df6b4cb38b6e8d87
Agent Andrew <agentandrewminecraft@gmail.com>
2024-03-07 15:21:42 -0600

    added get_coords function

--- utils/board_utils.py
+++ utils/board_utils.py
@@ -1,3 +1,8 @@
 def get_pos(rank: int, file: int, white_pov: bool) -> int:
     rank, file = (rank, file) if white_pov else (7 - rank, 7 - file)
     return (rank + 2) * 10 + file + 1
+
+
+def get_coords(pos: int) -> tuple[int, int]:
+    rank, file = divmod(pos, 10)
+    return rank - 2, file - 1
