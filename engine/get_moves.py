2f374eface9414e0035f05ec7803ebf8645bca41
Agent Andrew <agentandrewminecraft@gmail.com>
2024-03-08 15:26:03 -0600

    fixed generating move code + make piece stop when offscreen + made piece stop when hitting piece

--- engine/get_moves.py
+++ engine/get_moves.py
@@ -15,29 +15,32 @@ def get_moves(board: Board) -> list[Move]:
             pass
 
         else:
-            for dir in OFFSETS[piece.upper()]:
-                dest = pos + dir
+            for direction in OFFSETS[piece.upper()]:
+                # get the new destination and piece
+                dest = pos + direction
                 new_piece = board.board[dest]
 
-                # skip off board
-                if new_piece.isspace():
-                    continue
-
-                if new_piece == ".":
-                    moves.append(Move(pos, dest, False))
-
-                elif new_piece.isupper() != piece.isupper():
-                    moves.append(Move(pos, dest, True))
-
+                # if sliding piece
                 if piece.upper() in "BRQ":
-                    dest += dir
-                    while new_piece == "." or new_piece.isupper() != piece.isupper():
-                        if new_piece.isspace():
+                    while new_piece == "." or piece.isupper() != new_piece.isupper():
+                        # break if off the board
+                        if new_piece == " ":
                             break
 
                         if new_piece == ".":
+                            # add the sliding move and keep going through loop
                             moves.append(Move(pos, dest, False))
+                            dest += direction
+                            new_piece = board.board[dest]
                         else:
+                            # add the attacking move then break
                             moves.append(Move(pos, dest, True))
+                            break
+                else:
+                    # don't do anything if off the board
+                    if new_piece != " ":
+                        # add the piece if blank square or opposing colour
+                        if new_piece == "." or piece.isupper() != new_piece.isupper():
+                            moves.append(Move(pos, dest, new_piece == "."))
 
     return moves
