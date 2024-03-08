ef4d24769fe9a0c7c181634384b7d0ded5b06385
Agent Andrew <agentandrewminecraft@gmail.com>
2024-03-08 15:25:27 -0600

    simplified code by adding NE, NW, SE, and SW

--- engine/constants.py
+++ engine/constants.py
@@ -11,21 +11,13 @@ STARTING_BOARD = (
 
 # define directions
 N, E, S, W = -10, 1, 10, -1
+NE, NW, SE, SW = N + E, N + W, S + E, S + W
 
 # define piece offsets
 OFFSETS = {
-    "N": (
-        N * 2 + E,
-        N * 2 + W,
-        S * 2 + E,
-        S * 2 + W,
-        E * 2 + N,
-        E * 2 + S,
-        W * 2 + N,
-        W * 2 + S,
-    ),
-    "B": (N + E, E + S, S + W, W + N),
+    "N": (N + NE, N + NW, S + SE, S + SW, E + NE, E + SE, W + NW, W + SW),
+    "B": (NE, NW, SE, SW),
     "R": (N, E, S, W),
-    "Q": (N, N + E, E, E + S, S, S + W, W, W + N),
-    "K": (N, N + E, E, E + S, S, S + W, W, W + N),
+    "Q": (N, NE, E, SE, S, SW, W, NW),
+    "K": (N, NE, E, SE, S, SW, W, NW),
 }
