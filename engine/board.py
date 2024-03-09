f7661b16a2411826296b8c9369a7d195b785df5b
AgentAndrew <agentandrewminecraft@gmail.com>
2024-03-09 16:33:39 -0600

    added fen loading to get the board

--- engine/board.py
+++ engine/board.py
@@ -1,7 +1,18 @@
-from .constants import STARTING_BOARD
+DEFAULT_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
 
 
 class Board:
-    def __init__(self) -> None:
-        self.board = STARTING_BOARD
+    def __init__(self, fen: str = DEFAULT_FEN) -> None:
+        board = " " * 21
+        for char in fen:
+            if char.isdigit():
+                board += "." * int(char)
+            elif char == "/":
+                board += " " * 2
+                print(len(board))
+            else:
+                board += char
+        board += " " * 21
+
+        self.board = board
         self.white_move = True
