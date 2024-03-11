cf59f233ba19b9de725d970607e12b926ae6bb3b
Agent Andrew <agentandrewminecraft@gmail.com>
2024-03-11 15:29:12 -0500

    changed how castling is stored in the code

--- engine/move.py
+++ engine/move.py
@@ -1,7 +1,5 @@
 class Move:
-    def __init__(
-        self, pos: int, dest: int, capture: bool, castling: bool = False
-    ) -> None:
+    def __init__(self, pos: int, dest: int, capture: bool, castling: str = "") -> None:
         self.pos = pos
         self.dest = dest
         self.capture = capture
