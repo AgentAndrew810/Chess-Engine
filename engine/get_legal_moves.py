91d26f5cf21dfc4056b10aa39482e9e23036e92f
Agent Andrew <agentandrewminecraft@gmail.com>
2024-03-11 15:28:34 -0500

    simplified castling

--- engine/get_legal_moves.py
+++ engine/get_legal_moves.py
@@ -1,25 +1,7 @@
 from .board import Board
 from .get_moves import get_moves
 from .move import Move
-
-# positions for castling
-A1 = 91
-B1 = 92
-C1 = 93
-D1 = 94
-E1 = 95
-F1 = 96
-G1 = 97
-H1 = 98
-
-A8 = 21
-B8 = 22
-C8 = 23
-D8 = 24
-E8 = 25
-F8 = 26
-G8 = 27
-H8 = 28
+from .constants import E, W
 
 
 def get_legal_moves(board: Board) -> list[Move]:
@@ -32,27 +14,22 @@ def get_legal_moves(board: Board) -> list[Move]:
         king = "K" if board.white_move else "k"
         king_loc = child.board.index(king)
 
-        opp_moves = [move.dest for move in get_moves(child)]
+        opp_moves = [opp_move.dest for opp_move in get_moves(child)]
 
         if king_loc in opp_moves:
             continue
 
         if move.castling:
-            if move.dest == G1:
-                if F1 in opp_moves:
-                    continue
-            if move.dest == C1:
-                if D1 in opp_moves:
-                    continue
-            if move.dest == G8:
-                if F8 in opp_moves:
-                    continue
-            if move.dest == C8:
-                if D8 in opp_moves:
-                    continue
-
             if move.pos in opp_moves:
                 continue
+
+            if move.castling == "K":
+                if move.dest + E in opp_moves:
+                    continue
+
+            if move.castling == "Q":
+                if move.dest + W in opp_moves:
+                    continue
 
         moves.append(move)
     return moves
