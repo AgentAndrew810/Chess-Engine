5c40b525774f91c64d3ec6f6eefb9b573ecb18f1
Agent Andrew <agentandrewminecraft@gmail.com>
2024-03-11 15:29:00 -0500

    modified castling and added back constants

--- engine/get_moves.py
+++ engine/get_moves.py
@@ -1,68 +1,10 @@
 from .board import Board
 from .move import Move
-
-# define directions
-N, E, S, W = -10, 1, 10, -1
-NE, NW, SE, SW = -9, -11, 11, 9
-
-# define piece offsets
-OFFSETS = {
-    "N": (N + NE, N + NW, S + SE, S + SW, E + NE, E + SE, W + NW, W + SW),
-    "B": (NE, NW, SE, SW),
-    "R": (N, E, S, W),
-    "Q": (N, NE, E, SE, S, SW, W, NW),
-    "K": (N, NE, E, SE, S, SW, W, NW),
-}
-
-# positions for castling
-A1 = 91
-B1 = 92
-C1 = 93
-D1 = 94
-E1 = 95
-F1 = 96
-G1 = 97
-H1 = 98
-
-A8 = 21
-B8 = 22
-C8 = 23
-D8 = 24
-E8 = 25
-F8 = 26
-G8 = 27
-H8 = 28
+from .constants import OFFSETS, N, E, S, W, NE, NW, SE, SW
 
 
 def get_moves(board: Board) -> list[Move]:
     moves = []
-
-    # castling moves
-    if board.wk:
-        if board.board[F1] == "." and board.board[G1] == "." and board.board[H1] == "R":
-            moves.append(Move(E1, G1, False, True))
-
-    if board.wq:
-        if (
-            board.board[D1] == "."
-            and board.board[C1] == "."
-            and board.board[B1] == "."
-            and board.board[A1] == "R"
-        ):
-            moves.append(Move(E1, C1, False, True))
-
-    if board.bk:
-        if board.board[F8] == "." and board.board[G8] == "." and board.board[H8] == "r":
-            moves.append(Move(E8, G8, False, True))
-
-    if board.bq:
-        if (
-            board.board[D8] == "."
-            and board.board[C8] == "."
-            and board.board[B8] == "."
-            and board.board[A8] == "r"
-        ):
-            moves.append(Move(E8, C8, False, True))
 
     for pos, piece in enumerate(board.board):
         # skip blank squares and wrong colour
@@ -119,5 +61,32 @@ def get_moves(board: Board) -> list[Move]:
                         # add the piece if blank square or opposing colour
                         if new_piece == "." or piece.isupper() != new_piece.isupper():
                             moves.append(Move(pos, dest, new_piece != "."))
+
+            # castling
+            if piece == "K":
+                if board.castle[0]:
+                    if board.board[pos + E] == "." and board.board[pos + E * 2] == ".":
+                        moves.append(Move(pos, pos + E * 2, False, "K"))
+
+                if board.castle[1]:
+                    if (
+                        board.board[pos + W] == "."
+                        and board.board[pos + W * 2] == "."
+                        and board.board[pos + W * 3] == "."
+                    ):
+                        moves.append(Move(pos, pos + W * 2, False, "Q"))
+
+            if piece == "k":
+                if board.castle[2]:
+                    if board.board[pos + E] == "." and board.board[pos + E * 2] == ".":
+                        moves.append(Move(pos, pos + E * 2, False, "K"))
+
+                if board.castle[3]:
+                    if (
+                        board.board[pos + W] == "."
+                        and board.board[pos + W * 2] == "."
+                        and board.board[pos + W * 3] == "."
+                    ):
+                        moves.append(Move(pos, pos + W * 2, False, "Q"))
 
     return moves
