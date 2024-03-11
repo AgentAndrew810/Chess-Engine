bd8f22fdb9217f1fedfb50eb309ba778f6e5e858
Agent Andrew <agentandrewminecraft@gmail.com>
2024-03-11 15:28:08 -0500

    added back constants (changed my mind for the 10th time)

---
+++ engine/constants.py
@@ -0,0 +1,14 @@
+DEFAULT_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+
+# define directions
+N, E, S, W = -10, 1, 10, -1
+NE, NW, SE, SW = -9, -11, 11, 9
+
+# define piece offsets
+OFFSETS = {
+    "N": (N + NE, N + NW, S + SE, S + SW, E + NE, E + SE, W + NW, W + SW),
+    "B": (NE, NW, SE, SW),
+    "R": (N, E, S, W),
+    "Q": (N, NE, E, SE, S, SW, W, NW),
+    "K": (N, NE, E, SE, S, SW, W, NW),
+}
