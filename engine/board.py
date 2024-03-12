22e002c0b60a71d541434f5f5a44cae70b42baa0
Agent Andrew <agentandrewminecraft@gmail.com>
2024-03-12 15:37:39 -0500

    simplified code

--- engine/board.py
+++ engine/board.py
@@ -4,7 +4,6 @@ from .move import Move
 from .constants import DEFAULT_FEN, E, W
 
 
-
 class Board:
     def __init__(self, board: str, white_move: bool, castle: list[bool]) -> None:
         self.board = board
@@ -27,19 +26,14 @@ class Board:
 
     def make_move(self, move: Move) -> Board:
         board = list(self.board)
-        board[move.dest] = board[move.pos]
-        board[move.pos] = "."
+        board[move.pos], board[move.dest] = ".", board[move.pos]
 
         if move.castling == "K":
-            print("King Castling")
             board[move.pos + E] = board[move.dest + E]
             board[move.dest + E] = "."
 
         if move.castling == "Q":
-            print("Queen Castling")
             board[move.pos + W] = board[move.dest + W * 2]
             board[move.dest + W * 2] = "."
 
-        board = Board("".join(board), not self.white_move, self.castle)
-
-        return board
+        return Board("".join(board), not self.white_move, self.castle)
