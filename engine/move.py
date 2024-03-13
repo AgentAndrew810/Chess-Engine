d7e829a7a313bb1e9bc0d4b6563ca0703c6f1b54
AgentAndrew <agentandrewminecraft@gmail.com>
2024-03-12 22:15:08 -0500

    added storage for prom, double, and ep

--- engine/move.py
+++ engine/move.py
@@ -6,8 +6,13 @@ class Move:
         capture: bool = False,
         castling: str = "",
         prom: str = "",
+        double: bool = False,
+        ep: bool = False,
     ) -> None:
         self.pos = pos
         self.dest = dest
         self.capture = capture
         self.castling = castling
+        self.prom = prom
+        self.double = double
+        self.ep = ep
