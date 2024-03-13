8cbd6906d36135697a71c78c32a00a83d3127a01
AgentAndrew <agentandrewminecraft@gmail.com>
2024-03-12 23:14:15 -0500

    changed offsets to lists

--- engine/constants.py
+++ engine/constants.py
@@ -6,12 +6,12 @@ NE, NW, SE, SW = -9, -11, 11, 9
 
 # define piece offsets
 OFFSETS = {
-    "N": (N + NE, N + NW, S + SE, S + SW, E + NE, E + SE, W + NW, W + SW),
-    "B": (NE, NW, SE, SW),
-    "R": (N, E, S, W),
-    "Q": (N, NE, E, SE, S, SW, W, NW),
-    "K": (N, NE, E, SE, S, SW, W, NW),
+    "N": [N + NE, N + NW, S + SE, S + SW, E + NE, E + SE, W + NW, W + SW],
+    "B": [NE, NW, SE, SW],
+    "R": [N, E, S, W],
+    "Q": [N, NE, E, SE, S, SW, W, NW],
+    "K": [N, NE, E, SE, S, SW, W, NW],
 }
 
 # all the pieces a pawn can promote to
-PROM_PIECES = ("b", "n", "r", "q")
+PROM_PIECES = ["b", "n", "r", "q"]
