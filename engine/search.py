decfb440aa4c0ae802dae462d3a7ef58e42d128b
AgentAndrew <agentandrewminecraft@gmail.com>
2024-03-16 18:12:58 -0500

    added alpha beta pruning

--- engine/search.py
+++ engine/search.py
@@ -1,28 +1,46 @@
 from .get_legal_moves import get_legal_moves
 from .evaluate import evaluate
 from .board import Board
-from .move import Move
 
 
-def search(board: Board) -> Move | None:
-    return negamax(board, 2)[1]
-
-def negamax(board, depth):
+def search(board: Board, depth=3):
     moves = get_legal_moves(board)
-    
-    if depth == 0 or len(moves) == 0:
-        return evaluate(board), None
-    
-    max = -100000
-    max_move = None
-    
+    best_move = None
+
+    best_eval = float("-inf")
+    alpha = float("-inf")
+    beta = float("inf")
+
     for move in moves:
         child = board.make_move(move)
-        
-        score = -negamax(child, depth-1)[0]
-        
-        if score > max:
-            max = score
-            max_move = move
-            
-    return max, max_move
+        eval = -negamax(child, depth - 1, -beta, -alpha)
+
+        if eval > best_eval:
+            best_move = move
+            best_eval = eval
+
+        alpha = max(alpha, eval)
+        if alpha > beta:
+            break
+
+    return best_move
+
+
+def negamax(board: Board, depth, alpha, beta):
+    moves = get_legal_moves(board)
+
+    if depth == 0 or len(moves) == 0:
+        return evaluate(board)
+
+    value = float("-inf")
+
+    for move in moves:
+        child = board.make_move(move)
+
+        value = max(value, -negamax(child, depth - 1, -beta, -alpha))
+        alpha = max(alpha, value)
+
+        if alpha >= beta:
+            break
+
+    return value
