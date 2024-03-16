e642eccb593c090efdce1a2400e87dc9ee0c3737
AgentAndrew <agentandrewminecraft@gmail.com>
2024-03-16 18:38:56 -0500

    added move ordering

--- engine/search.py
+++ engine/search.py
@@ -4,8 +4,19 @@ from .board import Board
 from .move import Move
 
 
+def move_value(move: Move):
+    if move.capture:
+        return 0
+    elif move.prom:
+        return 1
+    elif move.castling:
+        return 2
+    return 3
+
+
 def search(board: Board, depth: int = 3) -> Move | None:
     moves = get_legal_moves(board)
+    moves = sorted(moves, key=lambda x: move_value(x))
     best_move = None
 
     best_eval = float("-inf")
@@ -29,6 +40,7 @@ def search(board: Board, depth: int = 3) -> Move | None:
 
 def negamax(board: Board, depth: int, alpha: float, beta: float) -> float:
     moves = get_legal_moves(board)
+    moves = sorted(moves, key=lambda x: move_value(x))
 
     if depth == 0 or len(moves) == 0:
         return evaluate(board)
