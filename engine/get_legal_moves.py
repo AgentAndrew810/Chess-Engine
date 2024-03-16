08638d7fc8a734a8fb254659b8339958eda16c51
AgentAndrew <agentandrewminecraft@gmail.com>
2024-03-16 15:24:17 -0500

    fixed bug with pawn check on castling

--- engine/get_legal_moves.py
+++ engine/get_legal_moves.py
@@ -14,15 +14,23 @@ def get_legal_moves(board: Board) -> list[Move]:
         king = "K" if board.white_move else "k"
         king_loc = child.board.index(king)
 
+        if move.castling:
+            dir = E if move.castling == "K" else W
+
+            # add temp king's to where the was and moves through in castling
+            # this ensures pawns can still attack these squares in opp_moves
+            temp_board = list(child.board)
+            temp_board[move.pos], temp_board[move.pos + dir] = king, king
+            child.board = "".join(temp_board)
+
+        # get all the square the opponent can move
         opp_moves = [opp_move.dest for opp_move in get_moves(child)]
 
-        # don't add move if opponent can attack king
+        # don't add move if opponent move to the king square (attack it)
         if king_loc in opp_moves:
             continue
 
         if move.castling:
-            dir = E if move.castling == "K" else W
-
             # don't add move if in check or moving through check
             if move.pos in opp_moves or move.pos + dir in opp_moves:
                 continue
