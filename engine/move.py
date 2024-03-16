b37b0645566e1532a94bce5e88c7420088ce214d
AgentAndrew <agentandrewminecraft@gmail.com>
2024-03-16 15:24:58 -0500

    added __slots__, added way to properly debug with move printing

--- engine/move.py
+++ engine/move.py
@@ -1,4 +1,9 @@
+import utils
+
+
 class Move:
+    __slots__ = "pos", "dest", "capture", "castling", "prom", "double", "ep"
+
     def __init__(
         self,
         pos: int,
@@ -16,3 +21,12 @@ class Move:
         self.prom = prom
         self.double = double
         self.ep = ep
+
+    def __repr__(self) -> str:
+        def chess_position(pos) -> str:
+            files = "abcdefgh"
+            ranks = "87654321"
+            rank, file = utils.get_coords(pos)
+            return files[file] + ranks[rank]
+
+        return chess_position(self.pos) + chess_position(self.dest) + self.prom
