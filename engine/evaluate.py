4391be879ade5cb2b5cdbfc644bdbef4dbd41f15
AgentAndrew <agentandrewminecraft@gmail.com>
2024-03-24 21:01:10 -0500

    simplified eval function

--- engine/evaluate.py
+++ engine/evaluate.py
@@ -1,15 +1,5 @@
-from utils import get_coords
 from .board import Board
-from .constants import POS_ON_BOARD, PIECE_VALUES, WMG_TABLES, WEG_TABLES
-
-BMG_TABLES = {}
-BEG_TABLES = {}
-
-for p, table in WMG_TABLES.items():
-    BMG_TABLES[p.lower()] = [row[::-1] for row in table]
-
-for p, table in WEG_TABLES.items():
-    BEG_TABLES[p.lower()] = [row[::-1] for row in table]
+from .constants import POS_ON_BOARD, PIECE_VALUES, MG_TABLES
 
 
 def evaluate(board: Board, middle_game: bool = True) -> int:
@@ -19,26 +9,14 @@ def evaluate(board: Board, middle_game: bool = True) -> int:
         p = board.board[pos]
 
         # skip blank squares
-        if p in " .":
+        if p == ".":
             continue
-
-        rank, file = get_coords(pos)
 
         if p.isupper():
             score += PIECE_VALUES[p]
-
-            # add middle or endgame piece table values
-            if middle_game:
-                score += WMG_TABLES[p][rank][file]
-            else:
-                score += WEG_TABLES[p][rank][file]
+            score += MG_TABLES[p][pos]
         else:
             score -= PIECE_VALUES[p.upper()]
-
-            # add middle or endgame piece table values
-            if middle_game:
-                score += BMG_TABLES[p][rank][file]
-            else:
-                score -= WEG_TABLES[p][rank][file]
+            score -= MG_TABLES[p][pos]
 
     return score if board.white_move else -score
