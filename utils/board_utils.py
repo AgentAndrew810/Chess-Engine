e74f1a8724062aa49ee1df035ce5d6b92ca96909
AgentAndrew <agentandrewminecraft@gmail.com>
2024-03-29 18:03:17 -0500

    added default to true for white_pov in board_utils

--- utils/board_utils.py
+++ utils/board_utils.py
@@ -1,4 +1,4 @@
-def get_pos(rank: int, file: int, white_pov: bool) -> int:
+def get_pos(rank: int, file: int, white_pov: bool = True) -> int:
     rank, file = (rank, file) if white_pov else (7 - rank, 7 - file)
     return (rank + 2) * 10 + file + 1
 
