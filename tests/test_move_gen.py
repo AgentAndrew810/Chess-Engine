9a7b23f4d0721387578969f16483ee997fac55a4
Agent Andrew <agentandrewminecraft@gmail.com>
2024-04-01 20:47:58 -0500

    renamed + changed how function works

---
+++ tests/test_move_gen.py
@@ -0,0 +1,31 @@
+import unittest
+import time
+
+import engine
+from .helper_functions import get_testing_data, fast_perft
+
+MAX_NODES = 10000
+
+
+class TestMoveGen(unittest.TestCase):
+    def test_move_gen(self) -> None:
+        start = time.time()
+        total_nodes = 0
+
+        for fen, depths in get_testing_data():
+            board = engine.Board(fen)
+            print(f"Searching: {fen}")
+
+            for depth, value in depths:
+                # skip depths that are too large
+                if value >= MAX_NODES:
+                    break
+
+                num = fast_perft(board, depth)
+                self.assertEqual(num, value)
+
+                total_nodes += num
+
+        end = time.time()
+        print(f"Total Nodes Found: {total_nodes}")
+        print(f"Speed: {round(total_nodes/(end-start))} nodes/sec")
