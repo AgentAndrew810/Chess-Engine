f94596900ccdfcd949c35f6927884b67e59eeba7
Agent Andrew <agentandrewminecraft@gmail.com>
2024-04-09 15:36:33 -0500

    added mate and stalemate detection, in_check extension

--- engine/search.py
+++ engine/search.py
@@ -1,4 +1,4 @@
-from .move_gen import move_gen
+from .move_gen import move_gen, get_pins_and_checks
 from .evaluate import evaluate
 from .board import Board
 from .move import Move
@@ -43,7 +43,24 @@ def negamax(board: Board, depth: int, alpha: float, beta: float) -> float:
     moves = move_gen(board)
     moves = sorted(moves, key=lambda x: move_value(x))
 
-    if depth == 0 or len(moves) == 0:
+    # determine if in check
+    king = "K" if board.white_move else "k"
+    king_pos = board.board.index(king)
+    in_check, _, _ = get_pins_and_checks(board.board, board.white_move, king_pos)
+
+    # add a depth of 1 if in check
+    if in_check:
+        depth += 1
+
+    if len(moves) == 0:
+        # if in check with no moves -> checkmate -> return super low score
+        if in_check:
+            return -100000 - depth
+        # not in check with no moves -> stalemate -> draw
+        else:
+            return 0
+
+    if depth == 0:
         return evaluate(board)
 
     value = float("-inf")
