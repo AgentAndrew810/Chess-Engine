3783473e87a5d5dfbc4634f2064ed57115362070
AgentAndrew <agentandrewminecraft@gmail.com>
2024-04-18 23:55:57 -0500

    added basic iterative deepening

--- engine/search.py
+++ engine/search.py
@@ -8,52 +8,39 @@ from .constants import MATE_SCORE
 
 
 class Engine:
-    def search(self, board: Board, depth: int = 4) -> Move | None:
+    def search(self, board: Board) -> Move | None:
         start = time.time()
 
         self.tt = {}
         self.nodes = 0
-        self.returned_tt_nodes = 0
-        self.tt_nodes = 0
+        eval = 0
+            
+        # iterative deepening
+        for depth in range(1, 1000):
+            eval = self.negamax(board, depth, float("-inf"), float("inf"))
 
-        moves = move_gen(board)
-        moves = sorted(moves, key=lambda x: self.move_value(x))
-        best_move = None
-
-        best_eval = float("-inf")
-        alpha = float("-inf")
-        beta = float("inf")
-
-        for move in moves:
-            board.make(move)
-
-            eval = -self.negamax(board, depth - 1, -beta, -alpha)
-
-            if eval > best_eval:
-                best_move = move
-                best_eval = eval
-
-            board.unmake(move)
-
-            alpha = max(alpha, eval)
-            if alpha >= beta:
+            # exit if a mate was found - makes it so the mate is actually played
+            # otherwise the mate would just be kept within the depth forever            
+            if eval == MATE_SCORE:
+                break
+            
+            if time.time()-start >= 1:
                 break
 
+        # get time taken
         time_taken = time.time() - start
         if time_taken == 0:
             speed = self.nodes
         else:
             speed = self.nodes / time_taken
-
-        if best_eval > 0:
-            eval = f"Computer is up {round(best_eval/100, 2)}"
-        else:
-            eval = f"Player is up {round(-best_eval/100, 2)}"
-
-        print(f"Time Taken: {round(time_taken, 3)}s - Speed: {round(speed)}nps - Total Nodes: {self.nodes} - Eval: {eval}")
-        print(f"TT Nodes: {self.tt_nodes} - TT Returned Nodes: {self.returned_tt_nodes}")
-
-        return best_move
+            
+        # format eval
+        eval = eval if board.white_move else -eval # switch to positive = white winning - negative = black winning rather than perspective of engine
+        eval_output = "+" if eval >= 0 else "-" # add positive and negative signs
+        eval_output += f"M{depth//2}" if abs(eval) == MATE_SCORE else str(eval) # add M if checkmate
+            
+        print(f"Nodes: {self.nodes} - Time: {round(time_taken, 3)}s - Speed: {round(speed)}nps - Depth: {depth} - Eval: {eval_output}")
+        return self.tt.get(board.zobrist, {"move": None})["move"]
 
     def move_value(self, move: Move) -> int:
         if move.prom:
@@ -61,29 +48,27 @@ class Engine:
         elif move.capture:
             return 1
         return 2
-
-    def negamax(self, board: Board, depth: int, alpha: float, beta: float) -> float:
+    
+    def negamax(self, board: Board, depth:int, alpha:float, beta:float) -> float:
         alpha_orig = alpha
         self.nodes += 1
+        
+        # tt_entry = self.tt.get(board.zobrist)
+        # if tt_entry is not None and tt_entry["depth"] >= depth:
+        #     if tt_entry["flag"] == "exact":
+        #         return tt_entry["value"]
+        #     elif tt_entry["flag"] == "lower":
+        #         alpha = max(alpha, tt_entry["value"])
+        #     elif tt_entry["flag"] == "upper":
+        #         beta = min(beta, tt_entry["value"])
 
-        tt_entry = self.tt.get(board.zobrist)
-        if tt_entry is not None and tt_entry["depth"] >= depth:
-            self.tt_nodes += 1
-            if tt_entry["flag"] == "exact":
-                self.returned_tt_nodes += 1
-
-                return tt_entry["value"]
-            elif tt_entry["flag"] == "lower":
-                alpha = max(alpha, tt_entry["value"])
-            elif tt_entry["flag"] == "upper":
-                beta = min(beta, tt_entry["value"])
-
-            if alpha >= beta:
-                return tt_entry["value"]
-
+            # if alpha >= beta:
+            #     return tt_entry["value"]
+            
+        # get and sort moves
         moves = move_gen(board)
         moves = sorted(moves, key=lambda x: self.move_value(x))
-
+        
         if len(moves) == 0:
             # determine if in check
             king = "K" if board.white_move else "k"
@@ -92,26 +77,32 @@ class Engine:
 
             # if in check with no moves -> checkmate -> return super low score
             if in_check:
-                return -MATE_SCORE - depth
+                return -MATE_SCORE
             # not in check with no moves -> stalemate -> draw
             else:
                 return 0
 
         if depth == 0:
             return evaluate(board)
-
-        value = float("-inf")
-
+        
+        best_value = float("-inf")
+        best_move = None
+        
         for move in moves:
             board.make(move)
-            value = -self.negamax(board, depth - 1, -beta, -alpha)
+            value = -self.negamax(board, depth-1, -beta, -alpha)
             alpha = max(alpha, value)
             board.unmake(move)
-
+            
+            if value >= best_value:
+                best_value = value
+                best_move = move
+            
             if alpha >= beta:
                 break
-
-        new_entry = {"depth": depth, "value": value}
+            
+        # store in transposition table
+        new_entry = {"depth": depth, "value": value, "move": best_move}
         if value <= alpha_orig:
             new_entry["flag"] = "upper"
         elif value >= beta:
@@ -120,4 +111,4 @@ class Engine:
             new_entry["flag"] = "exact"
         self.tt[board.zobrist] = new_entry
 
-        return alpha
+        return best_value
