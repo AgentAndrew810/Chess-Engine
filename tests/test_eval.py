ab892cfec3cd296a1c21bfff0c2dde832187863d
AgentAndrew <agentandrewminecraft@gmail.com>
2024-04-28 18:53:52 -0500

    test to eval

---
+++ tests/test_eval.py
@@ -0,0 +1,12 @@
+import unittest
+
+import engine
+from .helper_functions import get_testing_data
+
+class TestEval(unittest.TestCase):
+    def test_eval(self) -> None:
+        for fen, _ in get_testing_data():
+            board = engine.Board(fen)
+            eval = engine.evaluate(board)
+            board.white_move = not board.white_move
+            self.assertEqual(-eval, engine.evaluate(board))
