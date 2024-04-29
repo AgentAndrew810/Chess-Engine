28eea636085e6096bc73c4bad96435cb94443c0b
Agent Andrew <agentandrewminecraft@gmail.com>
2024-04-29 15:18:05 -0500

    removed slots and added equality checking (to find best move in list)

--- engine/move.py
+++ engine/move.py
@@ -4,8 +4,6 @@ from .utils import get_square
 
 
 class Move:
-    __slots__ = "pos", "dest", "capture", "castling", "prom", "double", "ep"
-
     def __init__(self, pos: int, dest: int, capture: bool = False, castling: str = "", prom: str = "", double: bool = False, ep: int = 0) -> None:
         self.pos = pos
         self.dest = dest
@@ -14,6 +12,9 @@ class Move:
         self.prom = prom
         self.double = double
         self.ep = ep
+
+    def __eq__(self, other: Move) -> bool:
+        return self.pos == other.pos and self.dest == other.dest and self.prom == other.prom
 
     def __repr__(self) -> str:
         return get_square(self.pos) + get_square(self.dest) + self.prom
