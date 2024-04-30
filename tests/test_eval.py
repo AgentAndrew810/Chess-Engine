4552d7ffe91414f195bd7ad815bac9929084f8dc
Agent Andrew <agentandrewminecraft@gmail.com>
2024-04-30 15:42:22 -0500

    added new test for eval

--- tests/test_eval.py
+++ tests/test_eval.py
@@ -3,6 +3,7 @@ import unittest
 import engine
 from .helper_functions import get_testing_data
 
+
 class TestEval(unittest.TestCase):
     def test_eval(self) -> None:
         for fen, _ in get_testing_data():
@@ -10,3 +11,7 @@ class TestEval(unittest.TestCase):
             eval = engine.evaluate(board)
             board.white_move = not board.white_move
             self.assertEqual(-eval, engine.evaluate(board))
+
+    def test_starting_evavl(self) -> None:
+        board = engine.Board()
+        self.assertEqual(engine.evaluate(board), 0)
