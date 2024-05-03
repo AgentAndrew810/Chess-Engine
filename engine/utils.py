7b424b2bc44df4df9060076a828e71a0646e8a6d
AgentAndrew <agentandrewminecraft@gmail.com>
2024-05-03 18:55:08 -0500

    improved utils for get_pos

--- engine/utils.py
+++ engine/utils.py
@@ -3,8 +3,8 @@ ranks = "87654321"
 
 
 def get_pos(square: str) -> int:
-    rank = ranks.index(square[0])
-    file = files.index(square[1])
+    file = files.index(square[0])
+    rank = ranks.index(square[1])
     return (rank + 2) * 10 + file + 1
 
 
