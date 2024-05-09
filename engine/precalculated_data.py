d8fde5b287eb10aec7152bd6c6db6c4cbdb1126d
Agent Andrew <agentandrewminecraft@gmail.com>
2024-05-09 15:20:01 -0500

    simplified pawn move gen code

--- engine/precalculated_data.py
+++ engine/precalculated_data.py
@@ -2,7 +2,7 @@ from .constants import N, S, OFFSETS, VALID_POS, PROM_PIECES
 from .constants import MG_VALUES, MG_TABLES_2D, EG_VALUES, EG_TABLES_2D
 from .move import Move
 
-# reverse table for the black piece (lowercase letter)
+# reverse piece tables for the black pieces (lowercase letter)
 for p in "PRQBKN":
     MG_TABLES_2D[p.lower()] = MG_TABLES_2D[p][::-1]
     EG_TABLES_2D[p.lower()] = EG_TABLES_2D[p][::-1]
@@ -34,7 +34,7 @@ for p, table in EG_TABLES_2D.items():
     new_table.extend([0] * 19)
 
     EG_TABLES[p] = new_table
-    
+
 # create tables of the moves each piece can make from each position
 MOVE_TABLES = {}
 LINE_OF_SIGHT = {}
@@ -42,38 +42,27 @@ LINE_OF_SIGHT_KNIGHT = {}
 
 for pos in VALID_POS:
     MOVE_TABLES[pos] = {}
-    
-    # white pawn forward moves
-    if pos // 10 == 8: # first rank
-        MOVE_TABLES[pos]["P"] = [Move(pos, pos+N), Move(pos, pos+N*2)]
-    elif pos//10 == 3: # last rank (is destination)
-        MOVE_TABLES[pos]["P"] = [Move(pos, pos+N, prom) for prom in PROM_PIECES]
-    else:
-        MOVE_TABLES[pos]["P"] = [Move(pos, pos + N)] if pos + N in VALID_POS else []
-        
-    # black pawn forward moves
-    if pos // 10 == 3: # first rank
-        MOVE_TABLES[pos]["p"] = [Move(pos, pos+S), Move(pos, pos+S*2)]
-    elif pos//10 == 8: # last rank (is destination)
-        MOVE_TABLES[pos]["p"] = [Move(pos, pos+S, prom) for prom in PROM_PIECES]
-    else:
-        MOVE_TABLES[pos]["p"] = [Move(pos, pos + S)] if pos + S in VALID_POS else []
-        
-    # white pawn attack moves
-    if pos // 10 == 3: # last rank (is destination)
-        MOVE_TABLES[pos]["Pa"] = {dir: [Move(pos, pos+dir, prom) for prom in PROM_PIECES] for dir in OFFSETS["Pa"] if pos+dir in VALID_POS}
-    else:
-        MOVE_TABLES[pos]["Pa"] = {dir: [Move(pos, pos+dir)] for dir in OFFSETS["Pa"] if pos+dir in VALID_POS}
-        
-    # black pawn attack moves
-    if pos // 10 == 8: # last rank (is destination)
-         MOVE_TABLES[pos]["pa"] = {dir: [Move(pos, pos+dir, prom) for prom in PROM_PIECES] for dir in OFFSETS["pa"] if pos+dir in VALID_POS}
-    else:
-        MOVE_TABLES[pos]["pa"] = {dir: [Move(pos, pos+dir)] for dir in OFFSETS["pa"] if pos+dir in VALID_POS}
+
+    for p, first_rank, last_rank, direction in [("P", 8, 3, N), ("p", 3, 8, S)]:
+        # pawn forward moves
+        if pos // 10 == first_rank:
+            MOVE_TABLES[pos][p] = [Move(pos, pos + direction), Move(pos, pos + direction * 2)]
+        elif pos // 10 == last_rank:
+            MOVE_TABLES[pos][p] = [Move(pos, pos + direction, prom) for prom in PROM_PIECES]
+        else:
+            MOVE_TABLES[pos][p] = [Move(pos, pos + direction)] if pos + direction in VALID_POS else []
+
+        # pawn attack moves
+        if pos // 10 == last_rank:
+            MOVE_TABLES[pos][p + "a"] = {
+                dir: [Move(pos, pos + dir, prom) for prom in PROM_PIECES] for dir in OFFSETS[p + "a"] if pos + dir in VALID_POS
+            }
+        else:
+            MOVE_TABLES[pos][p + "a"] = {dir: [Move(pos, pos + dir)] for dir in OFFSETS[p + "a"] if pos + dir in VALID_POS}
 
     # king and knight moves
     for p in "KN":
-        MOVE_TABLES[pos][p] = [Move(pos, pos+dir) for dir in OFFSETS[p] if pos+dir in VALID_POS]
+        MOVE_TABLES[pos][p] = [Move(pos, pos + dir) for dir in OFFSETS[p] if pos + dir in VALID_POS]
 
     # bishop, rook, and queen moves
     for p in "BRQ":
@@ -85,7 +74,7 @@ for pos in VALID_POS:
             while dest in VALID_POS:
                 MOVE_TABLES[pos][p][dir].append(Move(pos, dest))
                 dest += dir
-            
+
     # add line of sight for all directions and for knight
     LINE_OF_SIGHT[pos] = {dir: [move.dest for move in MOVE_TABLES[pos]["Q"][dir]] for dir in OFFSETS["Q"]}
     LINE_OF_SIGHT_KNIGHT[pos] = [move.dest for move in MOVE_TABLES[pos]["N"]]
