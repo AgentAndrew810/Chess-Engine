be6886db0455904b49912f7518d051a57914d9ed
Agent Andrew <agentandrewminecraft@gmail.com>
2024-05-13 15:40:56 -0500

    added doubled pawn, isolated pawn, and blocked pawn detection

--- engine/evaluate.py
+++ engine/evaluate.py
@@ -1,6 +1,6 @@
 from .board import Board
-from .constants import VALID_POS, GAME_PHASE_VALUE
-from .precalculated_data import MG_TABLES, EG_TABLES
+from .constants import VALID_POS, GAME_PHASE_VALUE, HALF_PAWN
+from .precalculated_data import MG_TABLES, EG_TABLES, PAWN_FILES, ADJ_PAWN_FILES, MOVE_TABLES
 
 
 def evaluate(board: Board) -> int:
@@ -14,6 +14,25 @@ def evaluate(board: Board) -> int:
             mg_value += MG_TABLES[p][pos]
             eg_value += EG_TABLES[p][pos]
             game_phase_value += GAME_PHASE_VALUE[p]
+
+            if p.upper() == "P":
+                # Doubled Pawn Detection
+                for pos2 in PAWN_FILES[pos]:  # loop through all squares in same file
+                    if p == board.board[pos2]:  # if it is a pawn of the same colour
+                        mg_value += -HALF_PAWN if p == "P" else HALF_PAWN
+
+                # Isolated Pawn Detection
+                num_ally_pawns = 0
+                for pos2 in ADJ_PAWN_FILES[pos]:  # loop through all squares in adjacent files
+                    if p == board.board[pos2]:  # if it is a pawn of the same colour:
+                        num_ally_pawns += 1
+
+                if num_ally_pawns == 0:  # if no other pawns of the same colour, give a penalty
+                    mg_value += -HALF_PAWN if p == "P" else HALF_PAWN
+
+                # Blocked Pawn Detection
+                if len(MOVE_TABLES[pos][p.upper()]) == 0:
+                    mg_value += -HALF_PAWN if board.white_move else HALF_PAWN
 
     mg_phase = min(game_phase_value, 24)  # in case of early promotion
     eg_phase = 24 - mg_phase
