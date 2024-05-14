7ceb62870a43d7830ef38d7196447ff0ee0a13c0
AgentAndrew <agentandrewminecraft@gmail.com>
2024-05-13 23:10:30 -0500

    used passed pawn and isolated pawn detection

--- engine/evaluate.py
+++ engine/evaluate.py
@@ -1,12 +1,13 @@
 from .board import Board
-from .constants import VALID_POS, GAME_PHASE_VALUE, HALF_PAWN
-from .precalculated_data import MG_TABLES, EG_TABLES, PAWN_FILES, ADJ_PAWN_FILES, MOVE_TABLES
+from .constants import VALID_POS, GAME_PHASE_VALUE, ISOLATED_PAWN_VALUE
+from .precalculated_data import MG_TABLES, EG_TABLES, ADJ_PAWN_FILES, PASSED_PAWNS, PASSED_PAWN_VALUE
 
 
 def evaluate(board: Board) -> int:
     game_phase_value = 0
     mg_value = 0
     eg_value = 0
+    value = 0
 
     for pos in VALID_POS:
         p = board.board[pos]
@@ -16,26 +17,24 @@ def evaluate(board: Board) -> int:
             game_phase_value += GAME_PHASE_VALUE[p]
 
             if p.upper() == "P":
-                # Doubled Pawn Detection
-                for pos2 in PAWN_FILES[pos]:  # loop through all squares in same file
-                    if p == board.board[pos2]:  # if it is a pawn of the same colour
-                        mg_value += -HALF_PAWN if p == "P" else HALF_PAWN
+                enemy_pawn = "p" if p == "P" else "P"
 
-                # Isolated Pawn Detection
-                num_ally_pawns = 0
-                for pos2 in ADJ_PAWN_FILES[pos]:  # loop through all squares in adjacent files
-                    if p == board.board[pos2]:  # if it is a pawn of the same colour:
-                        num_ally_pawns += 1
+                # passed pawn detection
+                for sq in PASSED_PAWNS[pos][p]:
+                    if board.board[sq] == enemy_pawn:
+                        break
+                else:
+                    value += PASSED_PAWN_VALUE[pos][p]
 
-                if num_ally_pawns == 0:  # if no other pawns of the same colour, give a penalty
-                    mg_value += -HALF_PAWN if p == "P" else HALF_PAWN
-
-                # Blocked Pawn Detection
-                if len(MOVE_TABLES[pos][p.upper()]) == 0:
-                    mg_value += -HALF_PAWN if board.white_move else HALF_PAWN
+                # isolated pawn detection
+                for sq in ADJ_PAWN_FILES[pos]:
+                    if p == board.board[sq]:  # if ally pawn
+                        break
+                else:
+                    value += ISOLATED_PAWN_VALUE[p]
 
     mg_phase = min(game_phase_value, 24)  # in case of early promotion
     eg_phase = 24 - mg_phase
-    score = (mg_value * mg_phase + eg_value * eg_phase) // 24
+    score = (mg_value * mg_phase + eg_value * eg_phase) // 24 + value
 
     return score if board.white_move else -score
