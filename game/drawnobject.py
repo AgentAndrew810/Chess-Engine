db8636d40cb3dad57fb54fc1fafead069e74ce25
AgentAndrew <agentandrewminecraft@gmail.com>
2024-05-18 18:10:47 -0500

    new ratios and sizes for drawn objects

--- game/drawnobject.py
+++ game/drawnobject.py
@@ -1,34 +1,50 @@
+units_in_width = 14.5
+units_in_height = 11
+ratio = units_in_width / units_in_height
+
+
 class DrawnObject:
     _instances = []
-    ratio = 21 / 18
 
     def __init__(self) -> None:
         self._instances.append(self)
 
     @classmethod
     def set_sizes(cls, screen_width: int, screen_height: int) -> None:
-        # if the width is bigger than the ratio, use the height to calculate the width
-        if screen_width >= screen_height * cls.ratio:
+        cls.screen_width = screen_width
+        cls.screen_height = screen_height
+
+        # if the width/height > ratio, use height to calculate width
+        if screen_width / screen_height > ratio:
             height = screen_height
-            width = round(height * cls.ratio)
-        # otherwise use the width to calculate height
+            width = round(height * ratio)
+        # otherwise use width to calculate height
         else:
             width = screen_width
-            height = round(width / cls.ratio)
+            height = round(width / ratio)
 
-        # use the height and width to calculate other sizes
-        cls.padd = round(width / 21)
-        cls.square_size = cls.padd * 2
-        cls.board_size = cls.square_size * 8
-        cls.line_size = round(cls.padd / 10)
+        # the size of one unit - this is the size used to determine where to place things on the gui
+        # a square on the chess board is unit x unit
+        cls.unit = round(width / units_in_width)
+
+        # calculate padding
+        # if width or height is too big, the other dimension gets more padding
+        cls.x_padd = cls.unit // 2 + (screen_width - width) // 2
+        cls.y_padd = cls.unit // 2 + (screen_height - height) // 2
+
+        # determine chess board sizes
+        cls.square_size = cls.unit
+        cls.board_size = cls.unit * 8
+        cls.profile_icon_size = cls.unit
+        cls.board_start_x = cls.x_padd
+        cls.board_end_x = cls.board_start_x + cls.board_size
+        cls.board_start_y = cls.y_padd + cls.profile_icon_size
+        cls.board_end_y = cls.board_start_y + cls.board_size
+
+        cls.line_size = round(cls.unit / 20)
         cls.piece_size = cls.square_size - cls.line_size * 2
 
-        # calculate the x_padd and y_padd based on if the height or width was adjusted
-        # add extra x_padd or y_padd depending on if the width or height is bigger than ratio
-        cls.x_padd = cls.padd + (screen_width - width) // 2
-        cls.y_padd = cls.padd + (screen_height - height) // 2
-
-        # if instance has an update method call it
-        for instance in cls._instances:
-            if hasattr(instance, "update"):
-                instance.update()
+        # if a child object has an update method call it
+        for child in cls._instances:
+            if hasattr(child, "update"):
+                child.update()
