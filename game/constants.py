2ff2370dcf2c8629f89e3e26c85a680af2d92263
AgentAndrew <agentandrewminecraft@gmail.com>
2024-05-18 15:22:25 -0500

    updated sizes and changed colours

--- game/constants.py
+++ game/constants.py
@@ -1,15 +1,19 @@
-# the default width / height of the window
-SCREEN_WIDTH = 840
-SCREEN_HEIGHT = 720
+# the default width & height of the window
+
+FACTOR = 75
+SCREEN_WIDTH = 16 * FACTOR
+SCREEN_HEIGHT = 12 * FACTOR
 
 # the minimum width / height of the window
-MIN_WIDTH = 410
-MIN_HEIGHT = 360
+MIN_WIDTH = SCREEN_WIDTH // 2
+MIN_HEIGHT = SCREEN_HEIGHT // 2
 
 # colours
 BLACK = (25, 23, 20)
-WHITE = (203,218,225)
-BLUE = (119,154,176)
+WHITE = (203, 218, 225)
+LIGHT = (240, 217, 181)
+DARK = (181, 136, 99)
+BLUE = (119, 154, 176)
 PINK = (255, 105, 180)
 DARK_PINK = (204, 76, 130)
 PURPLE = (138, 43, 226)
