ceab7f4a00405e3f20c27eccf772634c712d6922
AgentAndrew <agentandrewminecraft@gmail.com>
2024-05-18 20:01:21 -0500

    made a draw_transparent_object function

--- game/panel.py
+++ game/panel.py
@@ -1,6 +1,8 @@
 import pygame
 
+from engine import Board, Move
 from .drawnobject import DrawnObject
+from .utils import draw_transparent_object
 from .constants import WHITE, BLACK
 
 
@@ -15,33 +17,19 @@ class Panel(DrawnObject):
         panel_y = self.board_start_y
         panel_width = self.unit * 5
 
-        self.info_size = (panel_width, self.unit * 0.75)
         self.info_pos = (panel_x, panel_y)
+        self.info_size = (panel_width, round(self.unit * 0.75))
 
+        self.white_clock_pos = (panel_x, panel_y + round(self.unit * 0.75))
         self.white_clock_size = (panel_width // 2, self.unit)
-        self.white_clock_pos = (panel_x, panel_y + self.unit * 0.75)
+        self.black_clock_pos = (panel_x + panel_width // 2, panel_y + round(self.unit * 0.75))
         self.black_clock_size = (panel_width // 2, self.unit)
-        self.black_clock_pos = (panel_x + panel_width // 2, panel_y + self.unit * 0.75)
-        
-        self.moves_size = ()
 
-    def draw(self, screen: pygame.surface.Surface) -> None:
-        # draw the info bar
-        s = pygame.Surface(self.info_size)
-        s.set_alpha(200)
-        s.fill(BLACK)
-        screen.blit(s, self.info_pos)
+        self.moves_pos = (panel_x, panel_y + round(self.unit * 1.75))
+        self.moves_size = (panel_width, round(self.unit * 5.75))
 
-        # draw the clocks
-        s = pygame.Surface(self.white_clock_size)
-        s.set_alpha(200)
-        s.fill(WHITE)
-        screen.blit(s, self.white_clock_pos)
-
-        s = pygame.Surface(self.black_clock_size)
-        s.set_alpha(200)
-        s.fill(BLACK)
-        screen.blit(s, self.black_clock_pos)
-        
-        # moves played
-        
+    def draw(self, screen: pygame.surface.Surface, board: Board, past_moves: list[Move]) -> None:
+        draw_transparent_object(screen, BLACK, self.info_pos, self.info_size, 200)
+        draw_transparent_object(screen, WHITE, self.white_clock_pos, self.white_clock_size, 255)
+        draw_transparent_object(screen, BLACK, self.black_clock_pos, self.black_clock_size, 255)
+        draw_transparent_object(screen, BLACK, self.moves_pos, self.moves_size, 200)
--- game/utils.py
+++ game/utils.py
@@ -1,3 +1,15 @@
+import pygame
+
+
 def get_pos(rank: int, file: int, white_pov: bool = True) -> int:
     rank, file = (rank, file) if white_pov else (7 - rank, 7 - file)
     return (rank + 2) * 10 + file + 1
+
+
+def draw_transparent_object(
+    screen: pygame.surface.Surface, colour: tuple[int, int, int], position: tuple[int, int], size: tuple[int, int], alpha: int
+):
+    s = pygame.Surface(size)
+    s.set_alpha(alpha)
+    s.fill(colour)
+    screen.blit(s, position)
