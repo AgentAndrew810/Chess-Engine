f36d70509c4c50bef3919aecea3aa26490da70d4
Agent Andrew <agentandrewminecraft@gmail.com>
2024-05-29 15:31:01 -0500

    grow on hover

--- game/button.py
+++ game/button.py
@@ -9,15 +9,25 @@ class Button:
         self.height = height
 
         size = round(min(width, height) * 0.6)
+        big_size = round(min(width, height) * 0.7)
 
-        self.x_pos = x + (width - size) // 2
-        self.y_pos = y + (height - size) // 2
+        self.location = (x + (width - size) // 2, y + (height - size) // 2)
+        self.big_location = (x + (width - big_size) // 2, y + (height - big_size) // 2)
 
-        self.image = pygame.image.load(file)
-        self.image = pygame.transform.smoothscale(self.image, (size, size))
+        self.image = pygame.transform.smoothscale(pygame.image.load(file), (size, size))
+        self.big_image = pygame.transform.smoothscale(pygame.image.load(file), (big_size, big_size))
 
     def is_over(self) -> bool:
+        x, y = pygame.mouse.get_pos()
+
+        if self.x <= x <= self.x + self.width:
+            if self.y <= y <= self.y + self.width:
+                return True
+
         return False
 
     def draw(self, screen: pygame.surface.Surface) -> None:
-        screen.blit(self.image, (self.x_pos, self.y_pos))
+        if self.is_over():
+            screen.blit(self.big_image, self.big_location)
+        else:
+            screen.blit(self.image, self.location)
