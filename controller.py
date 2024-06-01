8aa6236a5fdc0c16e8dcdbbef23ea179d963a506
AgentAndrew <agentandrewminecraft@gmail.com>
2024-06-01 17:00:52 -0500

    added menu

--- controller.py
+++ controller.py
@@ -9,6 +9,17 @@ class GameController(game.DrawnObject):
     def __init__(self) -> None:
         super().__init__()
 
+        self.update()
+
+        self.menu = True
+        self.game_over = False
+        self.player_is_white = True
+        self.white_pov = True
+        self.x_offset = 0
+        self.y_offset = 0
+
+    def new_game(self) -> None:
+        # sets variables for new game
         self.board = engine.Board()
         self.computer = engine.Engine()
 
@@ -27,21 +38,27 @@ class GameController(game.DrawnObject):
         self.wstart = time.time()
         self.bstart = time.time()
 
-        self.update()
-
-        self.game_over = False
-        self.player_is_white = True
-        self.white_pov = True
-        self.x_offset = 0
-        self.y_offset = 0
-
     def update(self) -> None:
+        # determine position and sizes of menu buttons
+        x = self.x_padd + self.unit * 4
+        y = self.y_padd + self.unit * 3
+        width = self.unit * 6
+        height = self.unit
+
+        self.menu_buttons = {
+            "play-engine": game.MenuButton(x, y, width, height, "Play Engine", game.BLUE),
+            "play-friend": game.MenuButton(x, y + round(self.unit * 1.25), width, height, "Play Friend", game.BLUE),
+            "settings": game.MenuButton(x, y + round(self.unit * 2.5), width, height, "Settings", game.BLUE),
+            "quit": game.MenuButton(x, y + round(self.unit * 3.75), width, height, "Quit", game.BLUE),
+        }
+
+        # determine position and sizes of game buttons (specifically on the panel)
         x = self.x_padd + self.unit * 9
         y = self.y_padd + round(self.unit * 7.25)
         height = round(self.unit * 0.75)
         width = round(self.unit * 5 / 4)
 
-        self.buttons = {
+        self.panel_buttons = {
             "home": game.Button(x, y, width, height, "assets/home-icon.png"),
             "flip": game.Button(x + width, y, width, height, "assets/flip-icon.png"),
             "hint": game.Button(x + width * 2, y, width, height, "assets/hint-icon.png"),
@@ -50,6 +67,9 @@ class GameController(game.DrawnObject):
 
         self.background_image = pygame.image.load("assets/background.png")
         self.background_image = pygame.transform.smoothscale(self.background_image, (self.screen_width, self.screen_height))
+
+        self.logo_font = pygame.font.Font("assets/OpenSans.ttf", self.unit * 2)
+        self.button_font = pygame.font.Font("assets/OpenSans.ttf", self.unit // 2)
 
     @property
     def player_turn(self) -> bool:
@@ -63,25 +83,34 @@ class GameController(game.DrawnObject):
         return True
 
     def mouse_click(self, x: int, y: int) -> None:
-        # if the grab is inside the board
-        if not self.outside_board(x, y):
-            # if it is the players turn to move
-            if self.player_turn:
-                # get the rank and file grabbed and their offsets
-                rank, self.y_offset = divmod(y - self.y_padd, self.unit)
-                file, self.x_offset = divmod(x - self.x_padd, self.unit)
-
-                # get the pos and piece
-                pos = game.get_pos(rank, file, self.white_pov)
-                piece = self.board.board[pos]
-
-                # check if grabbing the correct colour
-                if piece.isalpha() and piece.isupper() == self.board.white_move:
-                    self.held_piece.grab(pos, piece, self.next_moves)
+        if self.menu:
+            # check for button presses
+            if self.menu_buttons["play-engine"].is_over():
+                self.menu = False
+                self.new_game()
         else:
-            # otherwise check for button presses
-            if self.buttons["flip"].is_over():
-                self.white_pov = not self.white_pov
+            # if the grab is inside the board
+            if not self.outside_board(x, y):
+                # if it is the players turn to move
+                if self.player_turn:
+                    # get the rank and file grabbed and their offsets
+                    rank, self.y_offset = divmod(y - self.y_padd, self.unit)
+                    file, self.x_offset = divmod(x - self.x_padd, self.unit)
+
+                    # get the pos and piece
+                    pos = game.get_pos(rank, file, self.white_pov)
+                    piece = self.board.board[pos]
+
+                    # check if grabbing the correct colour
+                    if piece.isalpha() and piece.isupper() == self.board.white_move:
+                        self.held_piece.grab(pos, piece, self.next_moves)
+            else:
+                # otherwise check for button presses
+                if self.panel_buttons["flip"].is_over():
+                    self.white_pov = not self.white_pov
+
+                elif self.panel_buttons["home"].is_over():
+                    self.menu = True
 
     def mouse_release(self, x: int, y: int) -> None:
         if self.outside_board(x, y):
@@ -145,7 +174,7 @@ class GameController(game.DrawnObject):
             self.game_over = True
 
     def update_clocks(self) -> None:
-        if not self.game_over:
+        if not self.menu and not self.game_over:
             if self.board.white_move:
                 self.wtime -= time.time() - self.wstart
                 self.wstart = time.time()
@@ -154,19 +183,31 @@ class GameController(game.DrawnObject):
                 self.bstart = time.time()
 
     def draw(self, screen: pygame.surface.Surface) -> None:
-        # screen.blit(self.background_image, (0, 0))
+        screen.blit(self.background_image, (0, 0))
 
-        self.board_gui.draw(
-            screen,
-            self.board,
-            self.white_pov,
-            self.held_piece,
-            self.past_moves[-1],
-            self.x_offset,
-            self.y_offset,
-        )
+        if self.menu:
+            # draw title
+            logo_text = self.logo_font.render("Chess Club 7", True, game.BLUE)
+            logo_rect = logo_text.get_rect(center=(self.x_padd + self.unit * 7, self.y_padd + self.unit // 2))
+            screen.blit(logo_text, logo_rect)
 
-        self.panel.draw(screen, self.wtime, self.btime, self.info_bar)
+            # draw buttons
+            for button in self.menu_buttons.values():
+                button.draw(screen, self.button_font)
 
-        for button in self.buttons.values():
-            button.draw(screen)
+        else:
+            self.board_gui.draw(
+                screen,
+                self.board,
+                self.white_pov,
+                self.held_piece,
+                self.past_moves[-1],
+                self.x_offset,
+                self.y_offset,
+            )
+
+            self.panel.draw(screen, self.wtime, self.btime, self.info_bar)
+
+            # draw buttons
+            for button in self.panel_buttons.values():
+                button.draw(screen)
