171d749cfbebbe4c21ebe0130707b39689520927
AgentAndrew <agentandrewminecraft@gmail.com>
2024-06-01 17:01:43 -0500

    changed colours

--- game/constants.py
+++ game/constants.py
@@ -8,14 +8,18 @@ MIN_WIDTH = SCREEN_WIDTH // 2
 MIN_HEIGHT = SCREEN_HEIGHT // 2
 
 # board colours
-LIGHT = (255, 255, 255)
-DARK = (25, 60, 90)
+LIGHT = (223, 238, 245)
+DARK = (50, 108, 146)
+
+# menu colours
+BLUE = (31, 81, 255)
 
 # highlight colours
 PINK = (255, 105, 180)
 DARK_PINK = (204, 76, 130)
 
 # panel colours
-DARK_GREY = (144, 162, 176)
-GREY = (199, 210, 215, 175)
+GREY = (144, 162, 176)
+LIGHT_GREY = (196, 206, 212, 200)
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
